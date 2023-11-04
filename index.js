38ea640110e184bd85e5e36e43e97468904b060b
Halal1122 <mdhealaluddin505@gmail.com>
2023-11-05 03:28:33 +0600

    id added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3x6azjv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,8 +31,27 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      console.log(page, size);
+        const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        
+        .toArray();
         res.send(result);
+    })
+
+    app.post('/productIds', async(req, res)=>{
+      const ids = req.body;
+      const idWithObjectId = ids.map(id=> new ObjectId(id))
+      const query = {
+        _id: {
+          $in: idWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      res.send(result)
     })
 
     app.get('/productsCount', async(req, res)=>{
